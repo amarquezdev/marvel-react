592d5c45fd9fd13093aba6d37b9ce712274daf90
Alejandro Marquez <al.marquezmiranda@gmail.com>
2024-02-22 22:27:53 -0300

    feat: adding images and routes

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Series from "./pages/Series";
+import Comics from "./pages/Comics";
+import Error404 from "./pages/Error404";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+        <Route path="/" element={<Home />} />
+          <Route path="/inicio" element={<Home />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="/comics" element={<Comics />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
